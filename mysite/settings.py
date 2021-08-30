d623a335d078bcf90b539097bc5ecb1bc9826347
pontakornth <pontakorn61@gmail.com>
2021-08-30 20:46:37 +0700

    chore: Config Django to use secret key and debug from .env file
    Working on #2

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3v#(9qp^=zi6csnrk=m8-(xh467g5e#2fv-(5j^c*)%hku$)l-'
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-3v#(9qp^=zi6csnrk=m8-(xh467g5e#2fv-(5j^c*)%hku$)l-')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
