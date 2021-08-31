62f780f1cb012267e8e3337880a29554cb4de7ae
pontakornth <pontakorn61@gmail.com>
2021-08-31 21:22:14 +0700

    feat: Add docstring and str representation
    Working on #6

--- polls/models.py
+++ polls/models.py
@@ -1,13 +1,28 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 
 # Create your models here.
 class Question(models.Model):
+    """Question of the poll"""
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        """Return true is the question is published less than 1 day from now."""
+
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
+    """Choice assigned to each question"""
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
