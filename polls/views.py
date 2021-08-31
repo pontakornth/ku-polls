fd23f5eff6c7d6dcdefe9bd9acb833e8b07b53cd
pontakornth <pontakorn61@gmail.com>
2021-08-31 21:53:13 +0700

    feat: Implementing list and question page
    Working on #3 and #4

--- polls/views.py
+++ polls/views.py
@@ -1,16 +1,25 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Question
 
 
 # Create your views here.
 def index(request):
     """Display available questions"""
-    return render(request, 'index.html')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return render(request, 'polls/index.html', context=context)
 
 
 def detail(request, question_id: int):
     """Display detail of the question"""
-    return HttpResponse(f"You're looking at question {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    context = {
+        'question': question
+    }
+    return render(request, 'polls/detail.html', context)
 
 
 def results(request, question_id: int):
