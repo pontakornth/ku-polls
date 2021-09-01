5ad164c43b3d24638ca85c95a295046dbc4e087b
pontakornth <pontakorn61@gmail.com>
2021-09-01 20:09:06 +0700

    feat: Implementing past question filter for detail view

--- polls/views.py
+++ polls/views.py
@@ -20,6 +20,10 @@ class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+    def get_queryset(self):
+        """Exclude unpublished questions"""
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 
 class ResultsView(generic.DetailView):
     """Question result page"""
