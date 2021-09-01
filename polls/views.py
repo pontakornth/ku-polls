eeaeefa449ec3c37dd3b9146ee0b12bcac81294e
pontakornth <pontakorn61@gmail.com>
2021-09-01 18:09:26 +0700

    feat: Refactor to generic views
    Close #11

--- polls/views.py
+++ polls/views.py
@@ -1,34 +1,30 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Question, Choice
+from django.views import generic
 
 
 # Create your views here.
-def index(request):
-    """Display available questions"""
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list
-    }
-    return render(request, 'polls/index.html', context=context)
+class IndexView(generic.ListView):
+    """Display latest questions"""
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return 5 latest questions"""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id: int):
-    """Display detail of the question"""
-    question = get_object_or_404(Question, pk=question_id)
-    context = {
-        'question': question
-    }
-    return render(request, 'polls/detail.html', context)
+class DetailView(generic.DetailView):
+    """Question detail page"""
+    model = Question
+    template_name = 'polls/detail.html'
 
 
-def results(request, question_id: int):
-    """Display the result of the question"""
-    question = get_object_or_404(Question, pk=question_id)
-    context = {
-        'question': question
-    }
-    return render(request, 'polls/results.html', context)
+class ResultsView(generic.DetailView):
+    """Question result page"""
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id: int):
