cf4b5d02c99e78eb1ba815e0e6c2cad0a47e4e91
pontakornth <pontakorn61@gmail.com>
2021-09-10 19:12:36 +0700

    fix: Incorrect default type

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-3v#(9qp^=zi6csnrk=m8-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=['localhost'], cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost', cast=Csv())
 
 
 # Application definition
