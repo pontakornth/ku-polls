8a4f04980f87b5847ccbcaf71ffd5959c534f7d0
pontakornth <pontakorn61@gmail.com>
2021-09-10 17:35:04 +0700

    chore: Externalize allowed hosts
    Working on #20

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from decouple import config
+from decouple import config, Csv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-3v#(9qp^=zi6csnrk=m8-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=['localhost'], cast=Csv())
 
 
 # Application definition
