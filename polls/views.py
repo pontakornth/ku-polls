da8dc5cbfd71e27d87f5d6ed2278e12ea9378ef3
pontakornth <pontakorn61@gmail.com>
2021-10-09 13:54:04 +0700

    refactor: Fix coding style according to flake8

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,4 @@
+"""Views for poll application."""
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
@@ -9,21 +10,31 @@ from django.views import generic
 
 # Create your views here.
 class IndexView(generic.ListView):
-    """Display latest questions"""
+    """Display latest questions."""
+
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return all available questions"""
+        """Return all available questions."""
         return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')
 
 
 class DetailView(generic.DetailView):
-    """Question detail page"""
+    """Question detail page."""
+
     model = Question
     template_name = 'polls/detail.html'
 
     def get(self, request, *args, **kwargs):
+        """
+        Get question object based on id.
+
+        If the question is already ended, it redirects to result view.
+        If the question is not available yet, it redirects to the home page
+        with an error message.
+
+        """
         try:
             self.object = self.get_object()
             if not self.object.can_vote():
@@ -35,16 +46,23 @@ class DetailView(generic.DetailView):
             return redirect(reverse('polls:index'))
 
     def get_queryset(self):
-        """Exclude unpublished questions"""
+        """Exclude unpublished questions."""
         return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
-    """Question result page"""
+    """Question result page."""
+
     model = Question
     template_name = 'polls/results.html'
 
     def get(self, request, *args, **kwargs):
+        """
+        Get question by id.
+
+        If there is no such question or it is not available,
+        it redirects to the home page with an error message.
+        """
         try:
             return super(ResultsView, self).get(request, *args, **kwargs)
         except Http404:
@@ -52,12 +70,12 @@ class ResultsView(generic.DetailView):
             return redirect(reverse('polls:index'))
 
     def get_queryset(self):
-        """Exclude unpublished questions"""
+        """Exclude unpublished questions."""
         return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 def vote(request, question_id: int):
-    """Vote on a question"""
+    """Vote on a question."""
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
